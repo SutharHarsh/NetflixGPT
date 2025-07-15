8e02219ddc4f90dbcb5f13d9dc9f147e3e7f2339
SutharHarsh <sutharharshp01@gmail.com>
2025-07-15 10:02:29 +0530

    Add route protection: restrict access to home for unauthenticated users and prevent signed-in users from visiting auth pages

--- NetflixGPT/src/components/Header.jsx
+++ NetflixGPT/src/components/Header.jsx
@@ -1,17 +1,42 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../store/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { LOGO } from "../utils/constant";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
         console.log("Sign-out successful.");
-        navigate("/");
       })
       .catch((error) => {
         // An error happened.
@@ -20,19 +45,16 @@ const Header = () => {
   };
   return (
     <div className="absolute w-full bg-gradient-to-b from-black z-10 flex justify-between items-center">
-      <img
-        className="w-44"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt=""
-      />
+      <img className="w-44" src={LOGO} alt="Logo" />
       {user && (
         <div className="flex justify-between items-center gap-4">
           <h1 className="text-white font-bold text-2xl">{user?.displayName}</h1>
           <div className="p-2">
             <img
-            className="w-20 rounded-[50%]" 
-            src={user?.photoURL} 
-            alt="profile_image" />
+              className="w-20 rounded-[50%]"
+              src={user?.photoURL}
+              alt="profile_image"
+            />
           </div>
           <button
             onClick={handleSignOut}
--- NetflixGPT/src/components/Login.jsx
+++ NetflixGPT/src/components/Login.jsx
@@ -7,15 +7,14 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../store/userSlice";
+import { PROFILE_IMAGE } from "../utils/constant";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const toggleSignIn = () => {
@@ -39,13 +38,13 @@ const Login = () => {
         password.current.value
       )
         .then((userCredential) => {
-          // Signed up
+          // SIGNED UP
           const user = userCredential.user;
-          console.log(user);
 
-          updateProfile(auth.currentUser, {
+          // PROFILE UPDATE LOGIC
+          updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/143863260?v=4",
+            photoURL: PROFILE_IMAGE,
           })
             .then(() => {
               // Profile updated!
@@ -58,7 +57,6 @@ const Login = () => {
                   photoURL: photoURL,
                 })
               );
-              navigate("/browse");
             })
             .catch((error) => {
               // An error occurred
@@ -71,7 +69,7 @@ const Login = () => {
           setErrorMessage(errorCode + "-" + errorMessage);
         });
     } else {
-      // sign in logic
+      // SIGN IN LOGIC
       signInWithEmailAndPassword(
         auth,
         email.current.value,
@@ -80,8 +78,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          console.log(user);
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -133,7 +129,7 @@ const Login = () => {
         <p className="text-red-600 font-bold py-2">{errorMessage}</p>
         <button
           onClick={onSubmit}
-          className="w-full bg-red-700 p-2 my-4 rounded-lg"
+          className="w-full bg-red-700 hover:bg-red-800 cursor-pointer p-2 my-4 rounded-lg"
         >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
